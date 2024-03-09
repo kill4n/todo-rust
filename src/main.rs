cb2190dcd67fd894bf7ec523431f3df4d65b3bf0
killan <crhistian.segura@gmail.com>
2024-03-09 17:11:02 -0500

    Add crossterm library and enable raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+    ExecutableCommand,
+};
+
+
 fn main() {
     println!("Hello, world!");
 }
